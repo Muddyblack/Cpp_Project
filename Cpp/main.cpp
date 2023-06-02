803676cd65cb92f04792a93487dfe97f57948d62
Muddyblack <Muddyblack03@gmail.com>
2023-06-02 22:02:50 +0200

    add shorter params

--- Cpp/main.cpp
+++ Cpp/main.cpp
@@ -7,9 +7,9 @@ namespace po = boost::program_options;
 
 void generateCode(const std::string &inputFileName, const std::string &outputDir)
 {
-    // Codegenerierung für die angegebene Datei hier implementieren
-    // Zum Beispiel: Datei öffnen, den Inhalt lesen und als Header- und Source-Datei speichern
-    // Speicherung im angegebenen Ausgabeverzeichnis
+    // Codegeneration implementation for the specified file goes here
+    // For example: Open the file, read its content, and save it as header and source files
+    // Save the files in the specified output directory
 }
 
 int main(int argc, char *argv[])
@@ -18,7 +18,7 @@ int main(int argc, char *argv[])
     std::string outputDir;
 
     po::options_description desc("Options");
-    desc.add_options()("input-file", po::value<std::string>(&inputFileName)->required(), "Input file")("output-dir", po::value<std::string>(&outputDir)->required(), "Output directory");
+    desc.add_options()("input-file,i", po::value<std::string>(&inputFileName)->required(), "Input file")("output-dir,o", po::value<std::string>(&outputDir)->required(), "Output directory");
 
     po::variables_map vm;
     po::store(po::parse_command_line(argc, argv, desc), vm);
