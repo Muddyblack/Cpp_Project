2a79d2438a7ed10b6e83d829d4855a7096f75d8d
Muddyblack <Muddyblack03@gmail.com>
2023-06-04 17:34:06 +0200

    Added Parameter checking

--- lib/main.cpp
+++ lib/main.cpp
@@ -1,11 +1,13 @@
 /**
- * @file main.cpp
- * @brief Main entry point for the Generating program.
  * This program generates C/C++ code from text input files in respect of the passed parameters.
  * This file mages all these parameters and helps the user to understand how all of this works.
+ *
+ * @file main.cpp
+ * @brief Main entry point for the Generating program.
  */
 
 #include <iostream>
+#include <conio.h>
 #include <unistd.h>
 #include <getopt.h>
 #include <regex>
@@ -34,21 +36,23 @@ private:
     std::string outputDir = PROJECT_PATH + "output\\"; /**< Output directory */
     std::string headerDir = outputDir + "include\\";   /**< Header file directory */
     std::string sourceDir = outputDir + "lib\\";       /**< Source file directory */
-    std::string outputType = "CPP";                    /**< Output file type (C or CPP) */
+    std::string outputType = "cpp";                    /**< Output file type (C or CPP) */
     std::string outputFilename = "main";               /**< Output filename (without extension) */
-    bool namespaceName;                                /**< Namespace yes or no (only for CPP) */
-    int signPerLine = -1;                              /**< Number of characters per line */
+    bool namespaceName = false;                        /**< Namespace yes or no (only for CPP) */
+    bool checkArgs = true;
+    int signPerLine = -1; /**< Number of characters per line */
 
     // Options
-    const static int options_amount = 9;
+    const static int options_amount = 10;
     const struct option longOptions[options_amount] = {
         {"output-dir", required_argument, nullptr, 'O'},
         {"headerdir", required_argument, nullptr, 'H'},
         {"sourcedir", required_argument, nullptr, 'S'},
         {"output-type", required_argument, nullptr, 't'},
         {"output-filename", required_argument, nullptr, 'f'},
-        {"namespace", required_argument, nullptr, 'n'},
+        {"namespace", no_argument, nullptr, 'n'},
         {"signperline", required_argument, nullptr, 'l'},
+        {"check", no_argument, nullptr, 'C'},
         {"help", no_argument, nullptr, 'h'},
         {nullptr, 0, nullptr, 0}};
 
@@ -104,7 +108,7 @@ private:
         std::cout << "Source Directory: " << CYAN_COLOR << sourceDir << RESET_COLOR << std::endl;
         std::cout << "Output Type: " << CYAN_COLOR << outputType << RESET_COLOR << std::endl;
         std::cout << "Output Filename: " << CYAN_COLOR << outputFilename << RESET_COLOR << std::endl;
-        std::cout << "Namespace Name: " << CYAN_COLOR << (namespaceName ? "Yes" : "No") << RESET_COLOR << std::endl;
+        std::cout << "Namespace Name: " << CYAN_COLOR << (namespaceName) << RESET_COLOR << std::endl;
         std::cout << "Sign Per Line: " << CYAN_COLOR << signPerLine << RESET_COLOR << std::endl;
         std::cout << std::endl;
     }
@@ -129,8 +133,9 @@ private:
         std::cout << "-S, --sourcedir <dir>     " << BLUE_COLOR << "Source file directory" << RESET_COLOR << "\n";
         std::cout << "-t, --output-type <type>  " << BLUE_COLOR << "Output file type (C or CPP)" << RESET_COLOR << "\n";
         std::cout << "-f, --output-filename <name>  " << BLUE_COLOR << "Output filename (without extension)" << RESET_COLOR << "\n";
-        std::cout << "-n, --namespace <name>        " << BLUE_COLOR << "Namespace yes/no" << RESET_COLOR << "\n";
+        std::cout << "-n, --namespace <name>        " << BLUE_COLOR << "Flag to use namespaces" << RESET_COLOR << "\n";
         std::cout << "-l, --signperline <number>    " << BLUE_COLOR << "Number of characters per line" << RESET_COLOR << "\n";
+        std::cout << "-C, --check                   " << BLUE_COLOR << "Flag to just create without checking the paths" << RESET_COLOR << "\n";
         std::cout << "-h, --help                    " << BLUE_COLOR << "Print help message" << RESET_COLOR << "\n";
 
         std::cout << GREEN_COLOR << "\n\n################################################################################\n";
@@ -144,6 +149,46 @@ private:
         std::cout << "#                                                                              #\n";
         std::cout << "################################################################################\n";
         std::cout << RESET_COLOR << std::endl;
+    }
+
+    /**
+     * Determine the programming language from string.
+     *
+     * @param input The input string to check.
+     * @return The detected programming language: "cpp" for C++, "c" for C
+     */
+    std::string checkLanguageType(const std::string &input)
+    {
+        if (input == "cpp" || input == "c++" || input == "g++")
+        {
+            return "cpp";
+        }
+        else if (input == "c")
+        {
+            return "c";
+        }
+
+        std::cout << RED_COLOR << "Cannot deterimine: " << BLUE_COLOR << "'" << input << "'" << RED_COLOR << " as a Language." << std::endl;
+        std::cout << "We have " << CYAN_COLOR << "c " << RED_COLOR << "or " << CYAN_COLOR << "cpp " << RED_COLOR << "as option" << RESET_COLOR << std::endl;
+        exit(1);
+    }
+
+    /**
+     * Checks if the provided file name is valid.
+     *
+     * @param fileName The file name to be validated.
+     * @return True if the file name is valid, false otherwise.
+     */
+    void isValidFileName(const std::string &fileName)
+    {
+        // Regular expression pattern for valid file name
+        std::regex pattern(R"([^\x00-\x1F\x7F\\/:*?"<>|]+)");
+
+        if (std::regex_match(fileName, pattern) == false)
+        {
+            std::cout << BLUE_COLOR << fileName << RED_COLOR << " is not a valid fileName" << RESET_COLOR << std::endl;
+            exit(1);
+        }
     }
 
     /**
@@ -182,22 +227,29 @@ private:
                 // isValidPath(optionName, sourceDir);
                 break;
             case 't':
-                outputType = optarg;
+                outputType = checkLanguageType(optarg);
                 break;
             case 'f':
                 outputFilename = optarg;
+                isValidFileName(outputFilename);
                 break;
             case 'n':
-                namespaceName = optarg;
+                if (outputType == "cpp")
+                {
+                    namespaceName = true;
+                }
                 break;
             case 'l':
                 signPerLine = std::stoi(optarg);
+                break;
+            case 'C':
+                checkArgs = false;
                 break;
             case 'h':
                 printHelpText();
                 exit(0);
             case '?':
-                if ((optopt == 'O' || optopt == 'H' || optopt == 'S' || optopt == 't' || optopt == 'f' || optopt == 'n' || optopt == 'l'))
+                if ((optopt == 'O' || optopt == 'H' || optopt == 'S' || optopt == 't' || optopt == 'f' || optopt == 'l'))
                 {
                     std::cout << ORANGE_COLOR << "OK ... option " << optionName << "' without argument"
                               << RESET_COLOR << std::endl;
@@ -241,7 +293,7 @@ private:
         }
         else
         {
-            std::cout << "Usage: program_name [options] input-file1 input-file2 ...\n";
+            std::cout << RED_COLOR << "Usage: program_name [options] input-file1 input-file2 ..." << RESET_COLOR << std::endl;
         }
     }
 
@@ -254,7 +306,12 @@ public:
     GenTxtSrcCode(int argc, char *argv[]) : argc(argc), argv(argv)
     {
         parseOptions();
-        printArguments();
+        if (checkArgs == true)
+        {
+            printArguments();
+            std::cout << GREEN_COLOR << "Press any key to continue..." << RESET_COLOR << std::endl;
+            _getch(); // Wait for any key
+        }
         codeGeneration();
     }
 };
