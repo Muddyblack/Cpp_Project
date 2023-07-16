7ca1532dc84222320746ff4bb292a72119fe3e05
Lucy <t3cktime@gmail.com>
2023-07-16 23:38:08 +0200

    Für Req 7 angepasst

--- tests/TESTGenTxtSrcCode.cpp
+++ tests/TESTGenTxtSrcCode.cpp
@@ -1,12 +1,30 @@
 #define BOOST_TEST_MODULE GenTxtSrcCodetests
 #include <boost/test/unit_test.hpp>
+#include <Helperfunctions.h>
+#include <ConsoleColors.h>
 #define private public
 #include <GenTxtSrcCode.h>
+//Creating a mock function to test private Method
+std::string checkLanguageType(std::string input){
+    const std::string input_lower = toLowerCase(input);
+    if (input_lower == "cpp" || input_lower == "c++" || input_lower == "g++")
+    {
+        return "cpp";
+    }
+    else if (input_lower == "c")
+    {
+        return "c";
+    }
+
+    BOOST_LOG_TRIVIAL(fatal) << RED_COLOR << "Cannot deterimine: " << BLUE_COLOR << "'" << input << "'" << RED_COLOR << " as a Language."
+                             << "\n"
+                             << "We have " << CYAN_COLOR << "c " << RED_COLOR << "or " << CYAN_COLOR << "cpp " << RED_COLOR << "as option" << RESET_COLOR << std::endl;
+    exit(1);
+}
+
 BOOST_AUTO_TEST_SUITE(GenTxtSrcCodeTestSuite)
 
     BOOST_AUTO_TEST_CASE(checkLanguagetypeTest){
-    char *argv[1] = {nullptr};
-    GenTxtSrcCode genTxtSrcCode(1,argv);
     //Input
     const std::vector<std::string> input ={"cpp","CPP",
                                            "c++","C++",
@@ -18,24 +36,12 @@ BOOST_AUTO_TEST_SUITE(GenTxtSrcCodeTestSuite)
                                                     "cpp","cpp",
                                                     "c","c"};
 
-    //genTxtSrcCode().checkLanguageType(input[0]);
     //Testing
         for (long long unsigned int i = 0; i < input.size(); ++i){
-           //BOOST_CHECK(genTxtSrcCode().checkLanguageType(input[i])==expectedoutput[i]);
+           std::string result = checkLanguageType(input[i]);
+           BOOST_CHECK(result==expectedoutput[i]);
         }
-
     }
-    BOOST_AUTO_TEST_CASE(isValisFileNameTest){
 
-
-    }
-    BOOST_AUTO_TEST_CASE(isValidNamespaceTest){
-
-
-    }
-    BOOST_AUTO_TEST_CASE(isValidVariableNameTest){
-
-
-    }
 
 BOOST_AUTO_TEST_SUITE_END()
--- tests/TESTHelperfunctions.cpp
+++ tests/TESTHelperfunctions.cpp
@@ -9,7 +9,8 @@ BOOST_AUTO_TEST_CASE(toLowerCaseTest)
     //Expected Output
     std::string expected = "this is a test";
     //Testing
-    BOOST_CHECK(expected == toLowerCase(input));
+    std::string result = toLowerCase(input);
+    BOOST_CHECK(expected == result);
 }
 
 BOOST_AUTO_TEST_CASE(toUpperCaseTest)
@@ -19,31 +20,28 @@ BOOST_AUTO_TEST_CASE(toUpperCaseTest)
     //Expected Output
     std::string expected = "THIS IS A TEST";
     //Testing
-    BOOST_CHECK(expected == toUpperCase(input));
+    std::string result = toUpperCase(input);
+    BOOST_CHECK(expected == result);
 }
 
 BOOST_AUTO_TEST_CASE(checkPathTest)
 {
     //Input
     std::string input = "/here/there/file";
-    std::cout << "input: " << input << std::endl;
     //Expected Output
     std::string expected = "C:\\\\here\\\\there\\\\file";
     //Testing
     std::string result = checkPath(input);
-    std::cout << "result: " << result << std::endl;
     BOOST_CHECK(result == expected);
 }
     BOOST_AUTO_TEST_CASE(checkPathTestfailure)
     {
         //Input
         std::string input = "./here/there/file";
-        std::cout << "input: " << input << std::endl;
         //Expected Output
         std::string expected = "C:\\\\here\\\\there\\\\file";
         //Testing
         std::string result = checkPath(input);
-        std::cout << "result: " << result << std::endl;
         BOOST_CHECK(result != expected);
     }
 
